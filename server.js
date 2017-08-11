dada7892429e7597821a6868dcf9b44433b1d8a7
akhtarzaman <akhtarzaman@users.noreply.github.com>
2017-08-11 09:03:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile('Article One Requested and will be handled by my server');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
