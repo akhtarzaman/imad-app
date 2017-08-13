4921a79511355f124cd7a0380e9f3fbaec7059ea
akhtarzaman <akhtarzaman@users.noreply.github.com>
2017-08-13 16:21:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,23 @@
 var button=document.getElementById('counter');
-var counter=0;
+
 
 button.onclick = function () {
     
-   // alert("I am an alert box!");
-    counter = counter +1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange=function() {
+        if ( request.readystate == XMLHttpRequest.DONE ) {
+            if ( request.status == 200) {
+                var counter = request.resposneText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+        
+    };
+    
+    request.open('GET','http://zamanakhtar.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
+   
 };
