eec60bebcea90f4f365ac5d5e007d1267b861d9e
akhtarzaman <akhtarzaman@users.noreply.github.com>
2017-08-13 15:43:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
-var button=document.getElementbyId("counter");
+var button=document.getElementbyId('counter');
 var counter=0;
 
 button.onclick = function () {
     
-    alert("I am an alert box!");
+   // alert("I am an alert box!");
     counter = counter +1;
-    var span = document.getElementById("count");
+    var span = document.getElementById('count');
     span.innerHTML=counter.toString();
 };
