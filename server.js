eba8eb3a9cab6f23daec009d1df54ff8936ae6a6
akhtarzaman <akhtarzaman@users.noreply.github.com>
2017-08-21 10:14:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 app.get('/article-one', function (req, res) {
   //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-  res.send("Article one is requested and will be served here");
+  res.send('Article one is requested and will be served here');
 });
 
 app.get('/article-one', function (req, res) {
