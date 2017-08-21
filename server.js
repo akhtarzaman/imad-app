6128c85ed18054fa98ca85acdc60ebab05016f21
akhtarzaman <akhtarzaman@users.noreply.github.com>
2017-08-21 09:50:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ });
+
+
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 8080;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
+
+
 /*
 var pool = require('pg').Pool;
 
@@ -13,8 +43,7 @@ var config = {
 };
 
 
-var app = express();
-app.use(morgan('combined'));
+
 
 var article={};
 function createTemplate(data) {
@@ -66,25 +95,3 @@ app.get('/article-three', function (req, res) {
 });
 */
 
-
-
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 8080;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
